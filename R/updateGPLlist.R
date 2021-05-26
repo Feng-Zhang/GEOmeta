558a7e177520295f433e1537566561f2b8693b74
章峰 <fengzhang0709@hotmail.com>
2021-05-26 14:27:54 +0800

    feat:add GPL anno

--- R/updateGPLlist.R
+++ R/updateGPLlist.R
@@ -5,23 +5,24 @@
 ##' @param GPL A character, the number of GSE.
 ##' @param probeID A character, the header of probe from specific GPL platform.
 ##' @param symbolID A character, the header of gene symbol from specific GPL platform.
-##' @return
+##' @return update the GPLlist.rda in data folder
 ##' @export
 ##' @examples
 ##' \dontrun{
-##' GPL="GPL10558"
-##' gplDat = Table(getGEO(GPL,getGPL=TRUE));head(gplDat)
-##' addAnno(GPL,probeID="ID",symbolID="ILMN_Gene")
+##' GPL="GPL13648"
+##' gplDat = Table(getGEO(GPL,getGPL=TRUE,destdir ="tmp"));colnames(gplDat)
+##' updateAnno(GPL,probeID="ID",symbolID="GENE_SYMBOL")
 ##' }
+##' @importFrom usethis use_data
 
-addAnno = function(GPL,probeID,symbolID){
+updateAnno = function(GPL,probeID,symbolID){
   rawRowN = nrow(GPLlist)
   if(!GPL %in%  GPLlist$GPL){
     temp=c(GPL,probeID,symbolID)
     GPLlist = rbind(GPLlist,temp)
     row.names(GPLlist)[GPLlist$GPL==GPL]=GPL
     if(nrow(GPLlist)>rawRowN){
-      usethis::use_data(GPLlist,overwrite=TRUE)
+      use_data(GPLlist,overwrite=TRUE)
     }
   }
 
